62579d2a9560cec2d7f95f77eb875b620dc5f4a6
enzoquarelo <enzo.quarelo@gmail.com>
2024-03-22 08:21:12 -0300

    método POST de médico finalizado + alteração na PacienteController

--- API-VitalHub/WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
+++ API-VitalHub/WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2fb6f683c188cd693e68fc3544e20682b3c1d13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de4af617184af81e11fa2d9a7ffce295c145d1aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
