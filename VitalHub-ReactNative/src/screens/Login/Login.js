c236e7678f58b00c3784eac23903014bf5dce526
Lucas Ribeiro De Lacerda <32784630803@info.local>
2024-03-26 09:28:01 -0300

    lucaslacerda

--- VitalHub-ReactNative/src/screens/Login/Login.js
+++ VitalHub-ReactNative/src/screens/Login/Login.js
@@ -1,7 +1,7 @@
 import { React, useState } from "react";
 
 //import dos components
-import { Container } from "../../components/Container/style"
+import { Container } from "../../components/Container/style";
 import { Logo } from "../../components/Logo/Logo";
 import { Title } from "../../components/Title/style";
 import { Input } from "../../components/Input/styles";
@@ -10,119 +10,168 @@ import { CustomButton, TitleButton } from "../../components/Button/styles";
 import { DefaultText } from "../../components/DefaultText/DefaultText";
 
 //imports de bibliotecas
-import { View, TouchableWithoutFeedback } from "react-native";
-import { AntDesign } from '@expo/vector-icons';
+import {
+  View,
+  TouchableWithoutFeedback,
+  ActivityIndicator,
+} from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 import { StatusBar } from "expo-status-bar";
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { userDecodeToken } from "../../utils/auth";
 
 //import da api
 import api from "../../service/service";
 
-
 export const Login = ({ navigation }) => {
-    const [email, setEmail] = useState('carlos.medico@gmail.com');
-    const [senha, setSenha] = useState('medico123');
-    
+  const [email, setEmail] = useState("carlos.medico@gmail.com");
+  const [senha, setSenha] = useState("medico123");
 
+  //const do Loading do botão Entrar
+  const [isLoading, setIsLoading] = useState(false);
 
-    async function Login() {
-        try {
-            //chama afunção de login pela url da api
-            const response = await api.post('/Login', {
-                email: email,
-                senha: senha
-            });
+  async function Login() {
+    try {
+      setIsLoading(true); // Define isLoading como true durante o carregamento
 
-            //valida se o método de login teve sucesso ou não
-            if (response.status === 200) {
-                //guarda o token em um constante e depois da AsyncStorage
-                const token = response.data.token;
-                await AsyncStorage.setItem("token", JSON.stringify(token));
+      // Simula um atraso de 3 segundos antes de fazer o login
+      await new Promise((resolve) => setTimeout(resolve, 3000));
 
-                //faz a navegação para a Main(Home)
-                navigation.navigate("Main");
-            } else {
-                //futuramente fazer um texto para usuario ou senha invalido caso o login falhar
-                alert('Login falhou');
-            }
+      //chama afunção de login pela url da api
+      const response = await api.post("/Login", {
+        email: email,
+        senha: senha,
+      });
 
-        } catch (error) {
-            console.error(error);
-        }
+      //valida se o método de login teve sucesso ou não
+      if (response.status === 200) {
+        //guarda o token em um constante e depois da AsyncStorage
+        const token = response.data.token;
+        await AsyncStorage.setItem("token", JSON.stringify(token));
+
+        //faz a navegação para a Main(Home)
+        navigation.navigate("Main");
+      } else {
+        //futuramente fazer um texto para usuario ou senha invalido caso o login falhar
+        alert("Login falhou");
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false); // Define isLoading como false após o carregamento
     }
+  }
 
-    //state e função para mostrar a senha
-    const [showPassword, setShowPassword] = useState(false);
+  //state e função para mostrar a senha
+  const [showPassword, setShowPassword] = useState(false);
 
-    const togglePasswordVisibility = () => {
-        setShowPassword(!showPassword);
-    }
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
-    return (
-        <Container>
-            <StatusBar />
-            <Logo />
+  return (
+    <Container>
+      <StatusBar />
+      <Logo />
 
-            <Title style={{ marginTop: 45 }}>Entrar ou criar conta</Title>
+      <Title style={{ marginTop: 45 }}>Entrar ou criar conta</Title>
 
-            <DefaultText
-                fontSize={18}
-                colorText={"#C81D25"}
-                widthText={"90%"}
-                textAlign={"start"}
-                style={{ marginTop: 20 }}
-            >
-                Usuário o Senha inválidos !
-            </DefaultText>
+      <DefaultText
+        fontSize={18}
+        colorText={"#C81D25"}
+        widthText={"90%"}
+        textAlign={"start"}
+        style={{ marginTop: 20 }}
+      >
+        Usuário o Senha inválidos !
+      </DefaultText>
 
-            <Input
-                placeholder="Usuário ou E-mail"
-                style={{ marginBottom: 20 }}
+      <Input
+        placeholder="Usuário ou E-mail"
+        style={{ marginBottom: 20 }}
+        value={email}
+        onChangeText={(txt) => setEmail(txt)}
+      />
 
-                value={email}
-                onChangeText={(txt) => setEmail(txt)}
-            />
+      <Container
+        heightContainer={"30px"}
+        flexDirection={"row"}
+        style={{ marginBottom: 10 }}
+      >
+        <Input
+          placeholder="Senha"
+          secureTextEntry={!showPassword}
+          value={senha}
+          onChangeText={(txt) => setSenha(txt)}
+        />
 
-            <Container heightContainer={"30px"} flexDirection={"row"} style={{ marginBottom: 10 }}>
-                <Input
-                    placeholder="Senha"
-                    secureTextEntry={!showPassword}
+        <TouchableWithoutFeedback onPress={togglePasswordVisibility}>
+          <AntDesign
+            name={showPassword ? "eyeo" : "eye"}
+            size={25}
+            color="#34898F"
+            style={{
+              position: "absolute",
+              right: "10%",
+            }}
+          />
+        </TouchableWithoutFeedback>
+      </Container>
 
-                    value={senha}
-                    onChangeText={(txt) => setSenha(txt)}
-                />
+      <Links
+        style={{ marginTop: 8, marginBottom: 35, textAlign: "start" }}
+        onPress={() => {
+          navigation.navigate("RecoverPassword");
+        }}
+      >
+        Esqueceu a senha?
+      </Links>
 
-                <TouchableWithoutFeedback onPress={togglePasswordVisibility}>
-                    <AntDesign
-                        name={showPassword ? "eyeo" : "eye"}
-                        size={25}
-                        color="#34898F"
-                        style={{
-                            position: 'absolute',
-                            right: "10%",
-                        }}
-                    />
-                </TouchableWithoutFeedback>
-            </Container>
+      <CustomButton onPress={isLoading ? null : Login} disabled={isLoading}>
+        {isLoading ? (
+          <ActivityIndicator size="small" color="#FFFFFF" />
+        ) : (
+          <TitleButton>ENTRAR</TitleButton>
+        )}
+      </CustomButton>
 
-            <Links style={{ marginTop: 8, marginBottom: 35, textAlign: 'start' }} onPress={() => { navigation.navigate("RecoverPassword") }}>Esqueceu a senha?</Links>
+      <CustomButton backgroundBtn={"#FFFFFF"} style={{ marginTop: 15 }}>
+        <AntDesign
+          style={{ marginRight: 20 }}
+          name="google"
+          size={20}
+          color="#496BBA"
+        />
+        <TitleButton colorTxt={"#496BBA"}>ENTRAR COM GOOGLE</TitleButton>
+      </CustomButton>
 
-            <CustomButton onPress={() => { Login() }}>
-                <TitleButton>ENTRAR</TitleButton>
-            </CustomButton>
-
-            <CustomButton backgroundBtn={"#FFFFFF"} style={{ marginTop: 15, }}>
-                <AntDesign style={{ marginRight: 20 }} name="google" size={20} color="#496BBA" />
-                <TitleButton colorTxt={"#496BBA"}>ENTRAR COM GOOGLE</TitleButton>
-            </CustomButton>
-
-            <Container style={{ marginTop: 10 }} widthContainer={"290px"} heightContainer={"30px"} flexDirection={"row"} justifyContent={"start"}>
-                <DefaultText fontFamily={"MontserratAlternates_600SemiBold"} fontSize={16} colorTxt={"#4E4B59"}>Não tem conta?</DefaultText>
-                <Links colorLink={"#496BBA"} fontLink={"MontserratAlternates_600SemiBold"} fontSize={16} onPress={() => { navigation.navigate("CreateAccount") }} style={{ textAlign: 'start' }}>Crie uma conta agora!</Links>
-            </Container>
-
-        </Container>
-    );
-}
+      <Container
+        style={{ marginTop: 10 }}
+        widthContainer={"290px"}
+        heightContainer={"30px"}
+        flexDirection={"row"}
+        justifyContent={"start"}
+      >
+        <DefaultText
+          fontFamily={"MontserratAlternates_600SemiBold"}
+          fontSize={16}
+          colorTxt={"#4E4B59"}
+        >
+          Não tem conta?
+        </DefaultText>
+        <Links
+          colorLink={"#496BBA"}
+          fontLink={"MontserratAlternates_600SemiBold"}
+          fontSize={16}
+          onPress={() => {
+            navigation.navigate("CreateAccount");
+          }}
+          style={{ textAlign: "start" }}
+        >
+          Crie uma conta agora!
+        </Links>
+      </Container>
+    </Container>
+  );
+};
--- VitalHub-ReactNative/src/screens/Profile/Profile.js
+++ VitalHub-ReactNative/src/screens/Profile/Profile.js
@@ -1,99 +1,130 @@
 import { React, useState, useEffect } from "react";
 
 import { StatusBar } from "expo-status-bar";
-import { ScrollView } from "react-native";
+import { ActivityIndicator, ScrollView } from "react-native";
 
-import { Container, ContainerInputAndTitle } from "../../components/Container/style"
-import { Title } from "../../components/Title/style"
-import { DefaultText } from "../../components/DefaultText/DefaultText"
-import { InputDisable, TitleInput } from "../../components/Input/styles"
+import {
+  Container,
+  ContainerInputAndTitle,
+} from "../../components/Container/style";
+import { Title } from "../../components/Title/style";
+import { DefaultText } from "../../components/DefaultText/DefaultText";
+import { InputDisable, TitleInput } from "../../components/Input/styles";
 import { UserImage } from "./style";
-import { ButtonDisable, CustomButton, TitleButton } from "../../components/Button/styles";
+import {
+  ButtonDisable,
+  CustomButton,
+  TitleButton,
+} from "../../components/Button/styles";
 
 import { userDecodeToken } from "../../utils/auth";
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const Profile = ({ navigation }) => {
-    //state para guardar e manipular os dados
-    const [userName, setUserName] = useState('');
-    const [userEmail, setUserEmail] = useState('');
+  //state para guardar e manipular os dados
+  const [userName, setUserName] = useState("");
+  const [userEmail, setUserEmail] = useState("");
 
-    //função que captura e guarda o nome e email do usuário pelo token
-    async function profileLoad(){
-        const token = await userDecodeToken();
+  //const do Loading do botão Entrar
+  const [isLoading, setIsLoading] = useState(false);
 
-        const userNameToken = token.name;
-        const userEmailToken = token.email;
-        setUserName(userNameToken);
-        setUserEmail(userEmailToken);
-    }
+  //função que captura e guarda o nome e email do usuário pelo token
+  async function profileLoad() {
+    const token = await userDecodeToken();
 
-    //função responsavél por deletar o token da AsyncStorage e retornar o usuário para a Login caso ele opte por sair do app
-    async function deleteToken() {
-        await AsyncStorage.removeItem('token');
+    setIsLoading(true); // Define isLoading como true durante o carregamento
 
-        navigation.navigate("Login")
+    // Simula um atraso de 3 segundos antes de fazer o login
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
-        console.log(token)
-    }
+    const userNameToken = token.name;
+    const userEmailToken = token.email;
+    setUserName(userNameToken);
+    setUserEmail(userEmailToken);
+    setIsLoading(false); // Define isLoading como false após o carregamento
+  }
 
-    useEffect(()=> {
-        profileLoad();
-    }, [])
+  //função responsavél por deletar o token da AsyncStorage e retornar o usuário para a Login caso ele opte por sair do app
+  async function deleteToken() {
+   
+    await AsyncStorage.removeItem("token");
 
-    return (
-        <ScrollView>
-            <Container>
-                <StatusBar style="light" />
+    
 
-                <UserImage source={require('../../../assets/images/doctorImage_temp.png')} />
+    navigation.navigate("Login");
 
-                <Title style={{marginTop: 14}}>{userName}</Title>
-                <DefaultText fontSize={18}>{userEmail}</DefaultText>
+    console.log(token);
+  }
 
-                <TitleInput style={{marginTop: 14, marginBottom: 5}}>Data de Nascimento</TitleInput>
-                <InputDisable
-                    placeholder="04/05/1999"
-                />
+  useEffect(() => {
+    profileLoad();
+  }, []);
 
-                <TitleInput style={{marginTop: 14, marginBottom: 5}}>CPF</TitleInput>
-                <InputDisable
-                    placeholder="983******"
-                />
+  return (
+    <ScrollView>
+      <Container>
+        <StatusBar style="light" />
 
-                <TitleInput style={{marginTop: 14, marginBottom: 5}}>Endereço</TitleInput>
-                <InputDisable
-                    placeholder="Rua Vicenso Silva, 987"
-                />
+        <UserImage
+          source={require("../../../assets/images/doctorImage_temp.png")}
+        />
 
-                <Container widthContainer={"90%"} heightContainer={"80px"} justifyContent={"space-between"} flexDirection={"row"} style={{marginTop: 14, marginBottom: 40}}>
-                    <ContainerInputAndTitle>
-                        <TitleInput>CEP</TitleInput>
-                        <InputDisable
-                            placeholder="06548-909"
-                        />
-                    </ContainerInputAndTitle>
+        <Title style={{ marginTop: 14 }}>{userName}</Title>
+        <DefaultText fontSize={18}>{userEmail}</DefaultText>
 
-                    <ContainerInputAndTitle>
-                        <TitleInput>Cidade</TitleInput>
-                        <InputDisable
-                            placeholder="Moema-SP"
-                        />
-                    </ContainerInputAndTitle>
-                </Container>
-                
-                <CustomButton style={{marginBottom: 20}}>
-                    <TitleButton>SALVAR</TitleButton>
-                </CustomButton>
+        <TitleInput style={{ marginTop: 14, marginBottom: 5 }}>
+          Data de Nascimento
+        </TitleInput>
+        <InputDisable placeholder="04/05/1999" />
 
-                <CustomButton>
-                    <TitleButton>EDITAR</TitleButton>
-                </CustomButton>
+        <TitleInput style={{ marginTop: 14, marginBottom: 5 }}>CPF</TitleInput>
+        <InputDisable placeholder="983******" />
 
-                <ButtonDisable widthButton={60} style={{marginTop: 20, marginBottom: 20}} onPress={() => {deleteToken()}}>
-                    <TitleButton>SAIR DO APP</TitleButton>
-                </ButtonDisable>
-            </Container>
-        </ScrollView>
-    );
-}
+        <TitleInput style={{ marginTop: 14, marginBottom: 5 }}>
+          Endereço
+        </TitleInput>
+        <InputDisable placeholder="Rua Vicenso Silva, 987" />
+
+        <Container
+          widthContainer={"90%"}
+          heightContainer={"80px"}
+          justifyContent={"space-between"}
+          flexDirection={"row"}
+          style={{ marginTop: 14, marginBottom: 40 }}
+        >
+          <ContainerInputAndTitle>
+            <TitleInput>CEP</TitleInput>
+            <InputDisable placeholder="06548-909" />
+          </ContainerInputAndTitle>
+
+          <ContainerInputAndTitle>
+            <TitleInput>Cidade</TitleInput>
+            <InputDisable placeholder="Moema-SP" />
+          </ContainerInputAndTitle>
+        </Container>
+
+        <CustomButton style={{ marginBottom: 20 }}>
+          <TitleButton>SALVAR</TitleButton>
+        </CustomButton>
+
+        <CustomButton>
+          <TitleButton>EDITAR</TitleButton>
+        </CustomButton>
+
+         {/* Botão "SAIR DO APP" com indicador de atividade */}
+         <ButtonDisable
+          widthButton={60}
+          style={{ marginTop: 20, marginBottom: 20 }}
+          onPress={isLoading ? null : deleteToken} // Impede a ação se isLoading for true
+          disabled={isLoading} // Desabilita o botão se isLoading for true
+        >
+          {isLoading ? (
+            <ActivityIndicator size="small" color="#FFFFFF" />
+          ) : (
+            <TitleButton>SAIR DO APP</TitleButton>
+          )}
+        </ButtonDisable>
+      </Container>
+    </ScrollView>
+  );
+};
