1b81ec7cc081ffe57578a3500e17e1bf3c2a5b2e
enzoquarelo <enzo.quarelo@gmail.com>
2024-03-27 08:13:27 -0300

    removendo os arquivos para fazer merge com enzoquarelo com a api com o gitignore

--- Projeto-VitalHub/src/components/Button/ButtonFooter.js
+++
@@ -1,9 +0,0 @@
-import { ButtonTabsStyle2, TextButton2 } from "./style";
-
-export const ButtonFooter = ({ textButton, clickButton = false, onPress }) => {
-    return (
-        <ButtonTabsStyle2 clickButton={clickButton} onPress={onPress}>
-            <TextButton2 clickButton={clickButton}>{textButton}</TextButton2>
-        </ButtonTabsStyle2>
-    );
-};
