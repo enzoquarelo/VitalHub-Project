b29a77080deaa13679ee3ab844cd7b4d55be3a00
enzoquarelo <enzo.quarelo@gmail.com>
2024-04-01 10:19:16 -0300

    enzoquarelobranch

--- API-VitalHub/WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
+++ API-VitalHub/WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34fc2296ff7affd710adb98d515b780b08b39ea5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6597051a33a51f76eec1325e5b33416ecfe56f75")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
