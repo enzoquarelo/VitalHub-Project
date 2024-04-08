4dedd026d323434befbda8de88e6f40f57af7c7e
enzoquarelo <enzo.quarelo@gmail.com>
2024-04-08 09:14:05 -0300

    subindo para merge

--- API-VitalHub/WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
+++ API-VitalHub/WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+228aff337b231180eb4b4f295e9231a209fc5efe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cce6002acbc4a8dbabdfab53c7007fc8179baa3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
