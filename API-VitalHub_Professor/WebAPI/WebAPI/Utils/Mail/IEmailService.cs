852726d664ce026afc41793463c64cc88e79497a
enzoquarelo <enzo.quarelo@gmail.com>
2024-04-22 10:31:01 -0300

    deletada a api antiga

--- API-VitalHub_Professor/WebAPI/WebAPI/Utils/Mail/IEmailService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebAPI.Utils.Mail
-{
-    public interface IEmailService
-    {
-        //método assincrono para envio de email que recebe o request
-        Task SendEmailAsync(MailRequest mailRequest);
-    }
-}
