9557b3eecf1e95ffb250ec46ef5e22319161003d
enzoquarelo <enzo.quarelo@gmail.com>
2024-04-22 10:37:37 -0300

    clear branch

--- VitalHub-ReactNative/src/components/Title/style.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const Title = styled.Text`
-    color: #33303E;
-
-    font-family: "MontserratAlternates_600SemiBold";
-    font-size: ${props => (props.fontSize ? props.fontSize + "px" : "24px")};
-`;
