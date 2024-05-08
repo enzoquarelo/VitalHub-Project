c1d9f55acd4cd70d4fbca335b697a05825ba071b
Lucas Ribeiro De Lacerda <32784630803@info.local>
2024-05-08 10:52:19 -0300

    telas de selacionar clinica,  médico, data, modal

--- VitalHub-ReactNative/src/components/Modais/CameraModal/CameraModal.js
+++ VitalHub-ReactNative/src/components/Modais/CameraModal/CameraModal.js
@@ -1,21 +1,22 @@
 import React, { useRef, useState, useEffect } from "react";
 import {
-  Modal,
-  StyleSheet,
-  StatusBar,
-  TouchableOpacity,
-  Text,
+    Modal,
+    StyleSheet,
+    StatusBar,
+    TouchableOpacity,
+    Text,
 } from "react-native";
 
 import {
-  BtnCapture,
-  BtnFlip,
-  ContainerButtonsCamera,
-  ConatinerImage,
-  Photo,
+    BtnCapture,
+    BtnFlip,
+    ContainerButtonsCamera,
+    ConatinerImage,
+    Photo,
 } from "./style";
 
-import { Camera, CameraType } from "expo-camera";
+import { CameraView, useCameraPermissions } from "expo-camera";
+
 import * as MediaLibrary from "expo-media-library";
 import * as ImagePicker from "expo-image-picker";
 
@@ -25,227 +26,226 @@ import { Container } from "../../Container/style";
 import { ButtonGaleria, LastPhoto } from "../../../screens/Profile/style";
 
 const ModalCamera = ({
-  visible,
-  onClose,
-  title,
-  onConfirm,
-  onPhotoConfirmed,
-  getMediaLibrary = false,
+    visible,
+    onClose,
+    title,
+    onConfirm,
+    onPhotoConfirmed,
+    getMediaLibrary = false,
 }) => {
-  const cameraRef = useRef(null);
+    const cameraRef = useRef(null);
 
-  const [photo, setPhoto] = useState(null);
-  const [openModal, setOpenModal] = useState(false);
-  const [cameraType, setCameraType] = useState(Camera.Constants.Type.back);
-  const [flashMode, setFlashMode] = useState(Camera.Constants.FlashMode.off);
-  const [flashOn, setFlashOn] = useState(false);
-  const [latesPhoto, setLatesPhoto] = useState(null); //salva a ultima foto
-  const [descricaoExame, setDescricaoExame] = useState(null);
+    const [photo, setPhoto] = useState(null);
+    const [openModal, setOpenModal] = useState(false);
+    const [cameraType, setCameraType] = useState('front');
+    const [flashMode, setFlashMode] = useState("off");
+
+    const [latestPhoto, setLatestPhoto] = useState(null);
+    const [permission, requestPermission] = useCameraPermissions();
+
+    async function selectImageGallery() {
+        console.log("selectImageGallery function called");
+
+        try {
+            const result = await ImagePicker.launchImageLibraryAsync({
+                mediaTypes: ImagePicker.MediaTypeOptions.Images,
+                quality: 1,
+            });
+
+            if (!result.cancelled) {
+                setPhoto(result.assets[0].uri);
+                setOpenModal(true);
+            }
+        } catch (error) {
+            console.error("Error in selectImageGallery:", error);
+        }
+    }
+
+    function toggleCameraFacing() {
+      setCameraType(current => (current === 'back' ? 'front' : 'back'));
+    }
   
 
-  async function SelectImageGallery() {
-    console.log("SelectImageGallery function called");
+    async function capturePhoto() {
+        if (cameraRef.current) {
+            const photo = await cameraRef.current.takePictureAsync({
+                quality: 1,
+            });
 
-    try {
-      const result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.Images,
-        quality: 1,
-      });
-
-      if (!result.canceled) {
-        setPhoto(result.assets[0].uri);
-        setOpenModal(true);
-      }
-    } catch (error) {
-      console.error("Error in SelectImageGallery:", error);
+            if (photo) {
+                setPhoto(photo.uri);
+                setOpenModal(true);
+            } else {
+                console.log("Failed to capture photo");
+            }
+        }
     }
-  }
 
-  async function CapturePhoto() {
-    
-    if (cameraRef.current) {
-      const photo = await cameraRef.current.takePictureAsync({quality: 1});
-
-      if (photo) {
-        setPhoto(photo.uri);
-        setOpenModal(true);
-      } else {
-        console.log("Falha ao capturar foto");
-      }
+    async function savePhoto() {
+        try {
+            if (photo) {
+                await MediaLibrary.saveToLibraryAsync(photo);
+                alert("Photo saved to gallery");
+                setOpenModal(false);
+                setLatestPhoto(photo.uri);
+                onClose();
+            } else {
+                alert("No image captured.");
+            }
+        } catch (error) {
+            console.error("Error saving photo:", error);
+            alert("Error saving photo.");
+        }
     }
-  }
 
-  //salvar foto
-  async function SavePhoto() {
-    try {
-      if (photo) {
-        await MediaLibrary.saveToLibraryAsync(photo); // Salva a imagem na galeria
-        alert("Foto salva na galeria"); // Exibe uma mensagem de sucesso
-        setOpenModal(false); // Fecha a modal da imagem
-        setDescricaoExame(photo.uri); // Atualiza o estado com o URI da foto salva
-        onClose(); // Retorna para a modal anterior
-        
-      } else {
-        alert("Nenhuma imagem capturada."); // Se não houver imagem capturada, exibe uma mensagem de erro
-      }
-    } catch (error) {
-      console.error("Erro ao salvar a foto:", error);
-      alert("Erro ao salvar a foto."); // Em caso de erro, exibe uma mensagem de erro
+    const toggleFlash = () => {
+        setFlashMode(flashMode === "on" ? "off" : "on",);
+    };
+
+    async function getLastPhoto() {
+        const { assets } = await MediaLibrary.getAssetsAsync({
+            sortBy: [[MediaLibrary.SortBy.creationTime, false]],
+            first: 1,
+        });
+
+        console.log(assets);
+        if (assets.length > 0) {
+            setLatestPhoto(assets[0].uri);
+        }
     }
-  }
 
-  
-  // Função para alternar o estado do flash
-  const toggleFlash2 = () => {
-    setFlashOn(!flashOn); // Alterna o estado do flash entre ligado e desligado
-    const newFlashMode = flashOn
-      ? Camera.Constants.FlashMode.off
-      : Camera.Constants.FlashMode.on;
-    setFlashMode(newFlashMode); // Altera o modo do flash com base no estado
-  };
+    useEffect(() => {
+        setPhoto(null);
 
-  async function GetLastPhoto() {
-    const { assets } = await MediaLibrary.getAssetsAsync({
-      sortBy: [[MediaLibrary.SortBy.creationTime, false]],
-      first: 1,
-    });
+        if (getMediaLibrary) {
+            getLastPhoto();
+        }
+    }, [visible]);
 
-    console.log(assets);
-    if (assets.length > 0) {
-      setLatesPhoto(assets[0].uri);
+    if (!permission) {
+      alert('carregando permissao')
     }
-  }
 
-  useEffect(() => {
-    setPhoto(null);
-
-    if (getMediaLibrary) {
-      GetLastPhoto();
-    }
-  }, [visible]);
-
-  return (
-    <>
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={visible}
-        onRequestClose={onClose}
-      >
-        <Camera
-          ref={cameraRef}
-          type={cameraType}
-          flashMode={flashMode}
-          style={styles.camera}
-          ratio={"16:9"}
-        >
-          <ContainerButtonsCamera>
-            <ButtonGaleria onPress={() => SelectImageGallery()}>
-              {latesPhoto != null ? (
-                <LastPhoto source={{ uri: latesPhoto }} />
-              ) : null}
-            </ButtonGaleria>
-
-            <BtnCapture onPress={CapturePhoto} />
-            <BtnFlip
-              onPress={() =>
-                setCameraType(
-                  cameraType === CameraType.front
-                    ? CameraType.back
-                    : CameraType.front
-                )
-              }
+    return (
+        <>
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={visible}
+                onRequestClose={onClose}
             >
-              <MaterialCommunityIcons
-                name="camera-flip"
-                size={35}
-                color="white"
-              />
-            </BtnFlip>
-          </ContainerButtonsCamera>
+                <CameraView
+                    ref={cameraRef}
+                    type={cameraType}
+                    flash={flashMode}
+                    style={styles.camera}
+                    ratio={"16:9"}
+                >
+                    <ContainerButtonsCamera>
+                        <ButtonGaleria onPress={() => selectImageGallery()}>
+                            {latestPhoto != null ? (
+                                <LastPhoto source={{ uri: latestPhoto }} />
+                            ) : null}
+                        </ButtonGaleria>
 
-          <TouchableOpacity
-            style={[
-              styles.cameraButtonText,
-              flashOn ? styles.flashButtonActive : styles.flashButtonInactive,
-            ]}
-            onPress={toggleFlash2}flash-off-sharp
-          >
-            <Ionicons
-              name={flashOn ? "flash" : "flash-off-sharp"}
-              size={24}
-              color={flashOn ? "black" : "black"}
-            />
-          </TouchableOpacity>
-        </Camera>
-        <StatusBar hidden={true} />
-      </Modal>
+                        <BtnCapture onPress={capturePhoto} />
+                        <BtnFlip onPress={toggleCameraFacing} >
+                            <MaterialCommunityIcons
+                                name="camera-flip"
+                                size={35}
+                                color="white"
+                            />
+                        </BtnFlip>
+                    </ContainerButtonsCamera>
 
-      {openModal && (
-        <Modal
-          animationType="slide"
-          visible={openModal}
-          onRequestClose={() => setOpenModal(false)}
-        >
-          <ConatinerImage>
-            <Photo source={{ uri: photo }} />
+                    <TouchableOpacity
+                        style={[
+                            styles.cameraButtonText,
+                            flashMode === "on"
+                                ? styles.flashButtonActive
+                                : styles.flashButtonInactive,
+                        ]}
+                        onPress={toggleFlash}
+                    >
+                        <Ionicons
+                            name={flashMode === "on" ? "flash" : "flash-off"}
+                            size={24}
+                            color={flashMode === "on" ? "black" : "black"}
+                        />
+                    </TouchableOpacity>
+                </CameraView>
+                <StatusBar hidden={true} />
+            </Modal>
 
-            <Container
-              heightContainer={45}
-              flexDirection={"row"}
-              justifyContent={"space-around"}
-              style={{ marginTop: 50, paddingLeft: 5, paddingRight: 5 }}
-            >
-              <ButtonDisable
-                onPress={() => setOpenModal(false)}
-                widthButton={40}
-              >
-                <TitleButton>REFAZER</TitleButton>
-              </ButtonDisable>
+            {openModal && (
+                <Modal
+                    animationType="slide"
+                    visible={openModal}
+                    onRequestClose={() => setOpenModal(false)}
+                >
+                    <ConatinerImage>
+                        <Photo source={{ uri: photo }} />
 
-              <CustomButton
-                onPress={() => {
-                  setOpenModal(false);
-                  onClose();
-                }}
-                widthButton={45}
-              >
-                <TitleButton onPress={SavePhoto}>CONFIRMAR</TitleButton>
-              </CustomButton>
-            </Container>
-          </ConatinerImage>
-        </Modal>
-      )}
-    </>
-  );
+                        <Container
+                            heightContainer={45}
+                            flexDirection={"row"}
+                            justifyContent={"space-around"}
+                            style={{
+                                marginTop: 50,
+                                paddingLeft: 5,
+                                paddingRight: 5,
+                            }}
+                        >
+                            <ButtonDisable
+                                onPress={() => setOpenModal(false)}
+                                widthButton={40}
+                            >
+                                <TitleButton>REFAZER</TitleButton>
+                            </ButtonDisable>
+
+                            <CustomButton
+                                onPress={() => {
+                                    setOpenModal(false);
+                                    onClose();
+                                }}
+                                widthButton={45}
+                            >
+                                <TitleButton onPress={savePhoto}>
+                                    CONFIRMAR
+                                </TitleButton>
+                            </CustomButton>
+                        </Container>
+                    </ConatinerImage>
+                </Modal>
+            )}
+        </>
+    );
 };
 
 export default ModalCamera;
 
 const styles = StyleSheet.create({
-  camera: {
-    flex: 1,
-    width: "100%",
-    height: "100%",
-  },
-  cameraButtonText: {
-    width: 80,
-    height: 45,
-    marginTop: 20,
-    marginLeft: 300,
-    color: "white",
-    fontSize: 18,
-    backgroundColor: "#496BBA",
-    padding: 10,
-    borderRadius: 5,
-    alignItems: "center",
-  },
-  // Estilo para o botão do flash quando está ativado
-  flashButtonActive: {
-    backgroundColor: "#F69C13", // Cor de fundo quando o flash está ativado
-  },
-  // Estilo para o botão do flash quando está desativado
-  flashButtonInactive: {
-    backgroundColor: "white", // Cor de fundo quando o flash está desativado
-  },
+    camera: {
+        flex: 1,
+        width: "100%",
+        height: "100%",
+    },
+    cameraButtonText: {
+        width: 80,
+        height: 45,
+        marginTop: 20,
+        marginLeft: 300,
+        color: "white",
+        fontSize: 18,
+        backgroundColor: "#496BBA",
+        padding: 10,
+        borderRadius: 5,
+        alignItems: "center",
+    },
+    flashButtonActive: {
+        backgroundColor: "#F69C13",
+    },
+    flashButtonInactive: {
+        backgroundColor: "white",
+    },
 });
